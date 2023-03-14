d793425fa4fd6b34a864a8abd63df1d221f647f0
Charles Bentley <cbentley@mail.ru>
2023-03-14 18:57:52 +0100

    comment and sort

--- src/pasa/cbentley/swing/images/ctx/ImgCtx.java
+++ src/pasa/cbentley/swing/images/ctx/ImgCtx.java
@@ -7,12 +7,11 @@ package pasa.cbentley.swing.images.ctx;
 import java.awt.image.BufferedImage;
 
 import pasa.cbentley.core.src4.ctx.ACtx;
-import pasa.cbentley.core.src4.ctx.UCtx;
 import pasa.cbentley.core.src5.ctx.C5Ctx;
 import pasa.cbentley.swing.ctx.SwingCtx;
 
 /**
- * Provides image services. It requires a SwingCtx.
+ * Provides image services. Logically it requires a {@link SwingCtx} .
  * 
  * <li> Animation support for {@link BufferedImage}
  * @author Charles Bentley
@@ -20,19 +19,26 @@ import pasa.cbentley.swing.ctx.SwingCtx;
  */
 public class ImgCtx extends ACtx {
 
-   private final SwingCtx sc;
+   public static final int CTX_ID = 4002;
 
+   private final SwingCtx  sc;
 
    public ImgCtx(SwingCtx sc) {
       super(sc.getUCtx());
       this.sc = sc;
    }
 
-   
+   /**
+    * 
+    * @return
+    */
    public C5Ctx getC5() {
       return sc.getC5();
    }
 
+   public int getCtxID() {
+      return CTX_ID;
+   }
 
    public SwingCtx getSwingCtx() {
       return sc;
